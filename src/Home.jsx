6a28ba8c940d49425d8047d6624cb808481cb970
Ganiyu Bashir <bashirganiyu@gmail.com>
2023-08-07 18:46:06 -0700

    11 commits

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,14 +17,14 @@ function Home() {
                             id= '1'
                             title='The lean startup'
                             price={29.99}
-                            image='/lean-start-up.jpg'
+                            image='images/lean-start-up.jpg'
                             rating={5}
                          />
                         <Product 
                             id= '2'
                             title='Petrol engine grinding machine'
                             price={129.99}
-                            image='/grinding-machine.jpg'
+                            image='images/grinding-machine.jpg'
                             rating={4}
                         />
                     </div>
@@ -34,21 +34,21 @@ function Home() {
                             id= '3'
                             title='Laptop Bag Pack'
                             price={5.99}
-                            image='/bag.jpg'
+                            image='images/bag.jpg'
                             rating={5}
                         />
                         <Product
                             id= '4'
                             title='iPhone 14 Pro Max, 1Terabyte ROM'
                             price={1999.99}
-                            image='/iPhone-14-promax.jpg'
+                            image='images/iPhone-14-promax.jpg'
                             rating={5}
                         />
                         <Product 
                             id= '5'
                             title='Benz 2023 Driverless Car With Petrol, Electric And Solar Powered'
                             price={65999.99}
-                            image='/benz-2023.jpg'
+                            image='images/benz-2023.jpg'
                             rating={5}
                         />
                     </div>
@@ -58,7 +58,7 @@ function Home() {
                             id= '6'
                             title='Sony Play Station 5 Console'
                             price={999.99}
-                            image='/sony-playstation5.jpg'
+                            image='images/sony-playstation5.jpg'
                             rating={5}
                         />
                     </div>
